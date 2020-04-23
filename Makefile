fc981da46e4250c77c5ed3c6fce8e63102e31b55
Ilya <solohin.i@gmail.com>
2020-04-23 12:00:04 +0300

    parseint

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 VERSION=0.2.1
 
 tag:
-
 	git tag ${VERSION}
 	git push --tags
--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     DC: process.env.DC || "nbg1",//TODO dc1,
-    EXPIRATION_TIMEOUT: 15 * 1000,
-    ERROR_EXPIRATION_TIMEOUT: 5 * 1000,
+    EXPIRATION_TIMEOUT: parseInt(process.env.EXPIRATION_TIMEOUT) || 15 * 1000,
+    ERROR_EXPIRATION_TIMEOUT: parseInt(process.env.ERROR_EXPIRATION_TIMEOUT) || 5 * 1000,
 }
